7964a314f69269249416a482121f5ba78f84604c
Gunnar Giffey <gunnar@textcontrol.com>
2015-05-12 17:56:18 +0200

    Added content.

---
+++ Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TXTextControl;
+
+namespace txspell_autocorrect
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textControl1_Changed(object sender, EventArgs e)
+        {
+            if (correctTWoINitialCApitalsToolStripMenuItem.Checked == false)
+                return;
+
+            // loop through all misspelled words
+            foreach (MisspelledWord word in textControl1.MisspelledWords)
+            {
+                // if the first two initials are uppercase
+                if (char.IsUpper(word.Text[0]) && char.IsUpper(word.Text[1]))
+                {
+                    // and the word is not mispelled
+                    txSpellChecker1.Check(word.Text.ToLower());
+
+                    // replace the word
+                    if (txSpellChecker1.IncorrectWords.Count == 0)
+                         textControl1.MisspelledWords.Remove(word, 
+                             ToUpperFirstLetter(word.Text));
+                }
+            }
+        }
+
+        // returns a string with the first letter uppercase
+        public static string ToUpperFirstLetter(string source)
+        {
+            if (string.IsNullOrEmpty(source))
+                return string.Empty;
+            
+            char[] letters = source.ToLower().ToCharArray();
+            letters[0] = char.ToUpper(letters[0]);
+            return new string(letters);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textControl1.Selection.Culture = new System.Globalization.CultureInfo("en-US");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace txspell_autocorrect
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+            Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo("en-US");
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
